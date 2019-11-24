5aac17d9c7b63e12bc3fc7da34db86ff49a81f7d
unknown <khaled.kareem0506@gmail.com>
2019-11-23 22:33:51 -0500

    adding created_by and updated_at fields to hydrant model. Updated the initial migration to account for this

--- hydrantBE/hydrantAPI/admin.py
+++ hydrantBE/hydrantAPI/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models.hydrant import Hydrant
 
-admin.site.register(Hydrant)
+class HydrantAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at', )
+
+admin.site.register(Hydrant, HydrantAdmin)
+
+# admin.site.register(Hydrant)
 
--- hydrantBE/hydrantAPI/models/hydrant.py
+++ hydrantBE/hydrantAPI/models/hydrant.py
@@ -5,5 +5,9 @@ class Hydrant(models.Model):
     description = models.CharField(max_length=1000)
     long = models.FloatField()
     lat = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+
     def __str__(self):
         return self.name
