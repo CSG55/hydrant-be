d428877fff4a989abda79faf59d297d8ee3d0954
unknown <khaled.kareem0506@gmail.com>
2019-11-24 15:07:19 -0500

    correcting typo with review_views serializer, adding migration for review table

--- hydrantBE/hydrantAPI/views/review_views.py
+++ hydrantBE/hydrantAPI/views/review_views.py
@@ -3,12 +3,12 @@ from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-from hydrantAPI.serializers import HydrantSerializer
+from hydrantAPI.serializers import ReviewSerializer
 from hydrantAPI.models.review import Review
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
-    serializer_class = HydrantSerializer
+    serializer_class = ReviewSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id']
