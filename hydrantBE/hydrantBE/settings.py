e53fd84452dc95405bb937d056204841733d7bb4
unknown <khaled.kareem0506@gmail.com>
2019-11-28 02:28:25 -0500

    reverting adminuser to authentication, as it was under permission classes

--- hydrantBE/hydrantBE/settings.py
+++ hydrantBE/hydrantBE/settings.py
@@ -47,8 +47,12 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser', 
+        'rest_framework.authentication.TokenAuthentication', 
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ]
+
 }
 
 MIDDLEWARE = [
